b6744375a4f1631593c58a39f742206cff4ea6b4
Debora <debbieblass@gmail.com>
2013-06-09 23:25:05 +0200

    solution from Monday class for nearerlarger

--- coding-test-2/practice-problems/lib/00_nearest_larger.rb
+++ coding-test-2/practice-problems/lib/00_nearest_larger.rb
@@ -1,57 +1,61 @@
 
-# Write a function, `nearest_larger(arr, i)` which takes an array and an
-# index.  The function should return another index, `j`: this should
-# satisfy:
-#
-# (a) `arr[i] < arr[j]`, AND
-# (b) there is no `j2` closer to `i` than `j` where `arr[i] < arr[j]`.
-#
-# In case of ties (see example beow), choose the earliest (left-most)
-# of the two indices. If no number in `arr` is largr than `arr[i]`,
-# return `nil`.
-# DEBBIE COMMENT: all tests passe except last one.
-# Go one folder above and run rake spec spec/00_nearest_larger_spec.rb
-# Difficulty: 2/5
-
-
 def nearest_larger(arr, idx)
-  i = idx
-  l = idx
-  r = idx
+  var = arr[idx]
+  distance = 1 
+  
+  begin 
+    left = idx - distance
+    right = idx + distance
+    
+    if left >= 0 && var < arr[left]
+      return left
+    elsif right < arr.length && var < arr[right]
+      return right 
+    end
+    distance += 1
+  end while left >= 0 && right < arr.length
+end
+
+
+
+# def nearest_larger(arr, idx)
+#   i = idx
+#   l = idx
+#   r = idx
 
    
-  while l >0
-    l -= 1
+#   while l >0
+#     l -= 1
 
-    if arr[l] > arr[i]
-      return l
-    else
-      false 
-    end  
-  end
+#     if arr[l] > arr[i]
+#       return l
+#     else
+#       false 
+#     end  
+#   end
 
 
-  while r <= arr.count
-    r += 1
+#   while r <= arr.count
+#     r += 1
 
-    if arr[r] > arr[i]
-      return r
-    else
-      false  
-    end 
-  end
+#     if arr[r] > arr[i]
+#       return r
+#     else
+#       false  
+#     end 
+#   end
   
 
-  if l >= r  
-    return l
-  elsif r > l
-    return r 
-  else
-    return nil
-  end       
+#   if l >= r  
+#     return l
+#   elsif r > l
+#     return r 
+#   else
+#     return nil
+#   end       
 
   
-end
+# end
 
 
 # puts nearest_larger([2,3,4,8], 2)
