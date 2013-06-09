46b524b076652e964eed704b08cf0c1a04eda506
Debora <debbieblass@gmail.com>
2013-06-09 23:18:55 +0200

    wonky coins has an endless loop :( that need to find still

--- coding-test-2/practice-problems/lib/04_wonky_coins.rb
+++ coding-test-2/practice-problems/lib/04_wonky_coins.rb
@@ -1,2 +1,28 @@
-def wonky_coins(n)
-end
+
+def wonky_coins(money)
+  array = []
+  
+  array[0] = money/2
+  array[1] = money/3
+  array[2] = money/4
+
+  loop do
+    array.each do |element|
+      array << element/2
+      array << element/3
+      array << element/4
+    end
+      array
+    # sum = 0
+    # break if array.each { |a| sum+=a } == 0
+    break if array.inject(0) { |sum, item| sum + item } == 0    
+  end
+
+  array.length
+end 
+
+
+# print wonky_coins(6)
+# print wonky_coins([3,5,6])
+
+# print wonky_coins(5)
